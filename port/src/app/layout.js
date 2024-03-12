8ddc3b4892223ec6760859db59b75784e60e4264
Vincent Cheung <wingchuncheung0108@gmail.com>
2024-03-12 11:22:14 +0800

    update icon

--- port/src/app/layout.js
+++ port/src/app/layout.js
@@ -19,6 +19,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>
         <div className="container">
           <Navbar />
